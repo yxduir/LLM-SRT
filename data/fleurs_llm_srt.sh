396092fa18c1944cd840cd6ccdc3395353e10e19
yxduir <yxduir@gmail.com>
2026-03-02 11:30:58 +0800

    20260302

--- data/fleurs_llm_srt.sh
+++ data/fleurs_llm_srt.sh
@@ -3,3 +3,4 @@ hf download ${data_config} --repo-type dataset --local-dir ./fleurs_eng_test_s2t
 if [ -f "./fleurs_eng_test_s2tt/audio.tar.gz" ]; then
         echo "解压音频文件..."
         tar -zxvf "./fleurs_eng_test_s2tt/audio.tar.gz" -C "./fleurs_eng_test_s2tt/"
+fi
--- data/multi30k_smt.sh
+++ data/multi30k_smt.sh
@@ -3,3 +3,4 @@ hf download ${data_config} --repo-type dataset --local-dir ./multi30k_test_tts_s
 if [ -f "./multi30k_test_tts_smt/audio.tar.gz" ]; then
         echo "解压音频文件..."
         tar -zxvf "./multi30k_test_tts_smt/audio.tar.gz" -C "./multi30k_test_tts_smt/"
+fi
